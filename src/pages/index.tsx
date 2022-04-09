2a4081fea5db49dae83338b88c39584f67f9418c
Gabriel Anatrone <anatrone.gabriel@gmail.com>
2022-04-09 05:59:57 -0300

    chore: nextjs, lint-staged, styled-components and others configurations

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { HomePage } from "~/pages/home/screens/home";
+
+const test = () => {
+  const test = "teste";
+  console.log("dsoiasdas");
+};
+
+const Index = () => <HomePage />;
+
+export default Index;
