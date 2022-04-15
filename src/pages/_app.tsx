873adb5f694a7fc58607a2ad8b1a47fd33b0c02f
Gabriel Anatrone <anatrone.gabriel@gmail.com>
2022-04-15 03:46:28 -0300

    chore: update nextjs core files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
-// import type { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 
 import { GlobalStyle } from "~/styles/global";
 import { theme } from "~/styles/theme";
-
-type AppProps = {
-  pageProps: any
-  Component: any
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
